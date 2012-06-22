80339c29a87d3a0018062453be594806a002cb6c
Mehdi Mirza <memirzamo@gmail.com>
2012-06-22 12:54:22 -0400

    group corruptor added

--- pylearn2/corruption.py
+++ pylearn2/corruption.py
@@ -6,6 +6,7 @@ training criterion.
 import numpy
 import theano
 from theano import tensor
+import theano.tensor.extra_ops
 T = tensor
 from theano.printing import Print
 # Shortcuts
@@ -65,11 +66,9 @@ class Corruptor(object):
     def corruption_free_energy(self, corrupted_X, X):
         raise NotImplementedError()
 
-
 class DummyCorruptor(Corruptor):
     def __call__(self, inputs):
         return inputs
-
 
 class BinomialCorruptor(Corruptor):
     """
@@ -106,7 +105,6 @@ class BinomialCorruptor(Corruptor):
             return self._corrupt(inputs)
         else:
             return [self._corrupt(inp) for inp in inputs]
-
 
 class BinomialCorruptorScaled(Corruptor):
     """
@@ -145,6 +143,51 @@ class BinomialCorruptorScaled(Corruptor):
         else:
             return [self._corrupt(inp) for inp in inputs]
 
+class BinomialCorruptorScaledGroup(Corruptor):
+    """
+    A binomial corruptor sets inputs to 0 with probability
+    0 < `corruption_level` < 1.
+    """
+    def __init__(self, corruption_level, group_size, rng=2001):
+
+        self.group_size = group_size
+        super(BinomialCorruptorScaledGroup, self).__init__(
+                corruption_level,
+                rng)
+
+    def _corrupt(self, x):
+        scale = self.corruption_level if self.corruption_level != 0 else 1
+        noise = self.s_rng.binomial(
+            size=(x.shape[0], self.group_size),
+            n=1,
+            p=1 - self.corruption_level,
+            dtype=theano.config.floatX)
+
+        noise = tensor.extra_ops.repeat(noise, x.shape[1] / self.group_size, axis =1)
+        return noise * x / scale
+
+    def __call__(self, inputs):
+        """
+        (Symbolically) corrupt the inputs with a binomial (masking) noise.
+
+        Parameters
+        ----------
+        inputs : tensor_like, or list of tensor_likes
+            Theano symbolic(s) representing a (list of) (mini)batch of inputs
+            to be corrupted, with the first dimension indexing training
+            examples and the second indexing data dimensions.
+
+        Returns
+        -------
+        corrupted : tensor_like
+            Theano symbolic representing the corresponding corrupted inputs,
+            where individual inputs have been masked with independent
+            probability equal to `self.corruption_level`.
+        """
+        if isinstance(inputs, tensor.Variable):
+            return self._corrupt(inputs)
+        else:
+            return [self._corrupt(inp) for inp in inputs]
 
 
 class GaussianCorruptor(Corruptor):
