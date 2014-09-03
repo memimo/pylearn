61f9537d1a03d52ede41fb4a2050245ac4c29fcf
Mehdi Mirza <memirzamo@gmail.com>
2014-09-03 15:55:46 -0700

    change l1, l2 coeffs to dictionary instead of a list

--- pylearn2/costs/mlp/__init__.py
+++ pylearn2/costs/mlp/__init__.py
@@ -4,6 +4,7 @@ Costs for use with the MLP model class.
 __authors__ = 'Vincent Archambault-Bouffard, Ian Goodfellow'
 __copyright__ = "Copyright 2013, Universite de Montreal"
 
+import warnings
 from theano import tensor as T
 
 from pylearn2.costs.cost import Cost, DefaultDataSpecsMixin, NullDataSpecsMixin
@@ -12,6 +13,7 @@ from pylearn2.utils.exc import reraise_as
 
 
 class Default(DefaultDataSpecsMixin, Cost):
+
     """
     The default Cost to use with an MLP.
 
@@ -41,6 +43,7 @@ class Default(DefaultDataSpecsMixin, Cost):
 
 
 class WeightDecay(NullDataSpecsMixin, Cost):
+
     """
     coeff * sum(sqr(weights))
 
@@ -48,8 +51,9 @@ class WeightDecay(NullDataSpecsMixin, Cost):
 
     Parameters
     ----------
-    coeffs : list
-        One element per layer, specifying the coefficient to multiply
+    coeffs : dict
+        Dictionary with layer names as its keys,
+        specifying the coefficient to multiply
         with the cost defined by the squared L2 norm of the weights for
         each layer.
 
@@ -77,25 +81,36 @@ class WeightDecay(NullDataSpecsMixin, Cost):
             added up for each set of weights.
         """
         self.get_data_specs(model)[0].validate(data)
+        assert T.scalar() != 0.  # make sure theano semantics do what I want
 
-        def wrapped_layer_cost(layer, coef):
+        def wrapped_layer_cost(layer, coeff):
             try:
                 return layer.get_weight_decay(coeff)
             except NotImplementedError:
-                if coef==0.:
+                if coef == 0.:
                     return 0.
                 else:
                     reraise_as(NotImplementedError(str(type(layer)) +
-                               " does not implement get_weight_decay."))
+                                                   " does not implement get_weight_decay."))
 
-        layer_costs = [ wrapped_layer_cost(layer, coeff)
-            for layer, coeff in safe_izip(model.layers, self.coeffs) ]
-
-        assert T.scalar() != 0. # make sure theano semantics do what I want
-        layer_costs = [ cost for cost in layer_costs if cost != 0.]
+        if isinstance(self.coeffs, list):
+            warnings.warn("Coefficients should be given as a dictionary "
+                          "with layer names as key. The support of "
+                          "coefficients as list would be deprecated from 03/03/2015")
+            layer_costs = [wrapped_layer_cost(layer, coeff)
+                           for layer, coeff in safe_izip(model.layers, self.coeffs)]
+            layer_costs = [cost for cost in layer_costs if cost != 0.]
+        else:
+            layer_costs = []
+            for layer in model.layers:
+                layer_name = layer.layer_name
+                if layer_name in self.coeffs:
+                    cost = wrapped_layer_cost(layer, self.coeffs[layer_name])
+                    if cost != 0.:
+                        layer_costs.append(cost)
 
         if len(layer_costs) == 0:
-            rval =  T.as_tensor_variable(0.)
+            rval = T.as_tensor_variable(0.)
             rval.name = '0_weight_decay'
             return rval
         else:
@@ -110,6 +125,7 @@ class WeightDecay(NullDataSpecsMixin, Cost):
 
 
 class L1WeightDecay(NullDataSpecsMixin, Cost):
+
     """
     coeff * sum(abs(weights))
 
@@ -117,10 +133,11 @@ class L1WeightDecay(NullDataSpecsMixin, Cost):
 
     Parameters
     ----------
-    coeffs : list
-        One element per layer, specifying the coefficient to multiply
-        with the cost defined by the L1 norm of the weights for each
-        layer.
+    coeffs : dict
+        Dictionary with layer names as its keys,
+        specifying the coefficient to multiply
+        with the cost defined by the squared L2 norm of the weights for
+        each layer.
 
         Each element may in turn be a list, e.g., for CompositeLayers.
     """
@@ -145,15 +162,29 @@ class L1WeightDecay(NullDataSpecsMixin, Cost):
             coeff * sum(abs(weights))
             added up for each set of weights.
         """
-        self.get_data_specs(model)[0].validate(data)
-        layer_costs = [ layer.get_l1_weight_decay(coeff)
-            for layer, coeff in safe_izip(model.layers, self.coeffs) ]
 
-        assert T.scalar() != 0. # make sure theano semantics do what I want
-        layer_costs = [ cost for cost in layer_costs if cost != 0.]
+        assert T.scalar() != 0.  # make sure theano semantics do what I want
+        self.get_data_specs(model)[0].validate(data)
+
+        if isinstance(self.coeffs, list):
+            warnings.warn("Coefficients should be given as a dictionary "
+                          "with layer names as key. The support of "
+                          "coefficients as list would be deprecated from 03/03/2015")
+            layer_costs = [layer.get_l1_weight_decay(coeff)
+                           for layer, coeff in safe_izip(model.layers, self.coeffs)]
+            layer_costs = [cost for cost in layer_costs if cost != 0.]
+
+        else:
+            layer_costs = []
+            for layer in model.layers:
+                layer_name = layer.layer_name
+                if layer_name in self.coeffs:
+                    cost = layer.get_l1_weight_decay(self.coeffs[layer_name])
+                    if cost != 0.:
+                        layer_costs.append(cost)
 
         if len(layer_costs) == 0:
-            rval =  T.as_tensor_variable(0.)
+            rval = T.as_tensor_variable(0.)
             rval.name = '0_l1_penalty'
             return rval
         else:
